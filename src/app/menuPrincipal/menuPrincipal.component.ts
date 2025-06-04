268e7cd7e0ffee4d7a13d61c7b7ca994a1407d82
FenF3X <adrmar3@alu.edu.gva.es>
2025-06-04 09:01:40 +0200

    aplicacion para movil

--- src/app/menuPrincipal/menuPrincipal.component.ts
+++ src/app/menuPrincipal/menuPrincipal.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-principal',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './menuPrincipal.component.html',
   styleUrl: './menuPrincipal.component.scss'
 })
-export class MenuPrincipalComponent {
+export class MenuPrincipalComponent implements OnInit {
+
+  libros:any[] = [];
+  
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+     this.http.get<any[]>('http://localhost:3000/datos')
+      .subscribe(data => {
+        this.libros = data;
+      }, error => {
+        console.error('Error al obtener los libros:', error);
+      });
+    }
 
 }
