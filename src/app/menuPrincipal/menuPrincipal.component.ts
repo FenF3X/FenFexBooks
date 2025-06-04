9add127ef06a540031ee235515cb0fbfdd8e479c
FenF3X <adrmar3@alu.edu.gva.es>
2025-06-04 08:21:35 +0200

    creación componente Menu

---
+++ src/app/menuPrincipal/menuPrincipal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-principal',
+  standalone: false,
+  templateUrl: './menuPrincipal.component.html',
+  styleUrl: './menuPrincipal.component.scss'
+})
+export class MenuPrincipalComponent {
+
+}
